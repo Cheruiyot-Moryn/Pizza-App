327dbca575782d2c959f1f09e0e319823b8cd9fd
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-03-18 15:41:43 +0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+$(document).ready(function(){
+    // $("button.proceed").click(function(){
+    //   $("button.proceed").hide();
+    //   $("#information").hide();
+    //   $("div.choise").slideDown(1000);
+    // });
+    $("button.proceed").click(function(event){
+     let pizzaname = $(".name option:selected").val();
+     let pizzasize = $("#size option:selected").val();
+     let pizzacrust = $("#crust option:selected").val();
+     let pizzatopping = [];
+     $.each($("input[name='toppings']:checked"), function(){            
+         pizzatopping.push($(this).val());
+     });
+     console.log(pizzatopping.join(", "));
+  
