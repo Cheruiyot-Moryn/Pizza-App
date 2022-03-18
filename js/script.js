5f14420f8b410df8b2b85c41754ce9abcf32f45e
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-03-18 15:48:24 +0300

    add button on script.js

--- js/script.js
+++ js/script.js
@@ -82,4 +82,61 @@ $(document).ready(function(){
     $("#pizzacrust").html($("#crust option:selected").val());
     $("#pizzatopping").html(ptopping.join(", "));
     $("#totals").html(total);
+
+    // Add pizza button
+    $("button.addPizza").click(function(){
+        let pizzaname = $(".name option:selected").val();
+        let pizzasize = $("#size option:selected").val();
+        let pizzacrust = $("#crust option:selected").val();
+        let pizzatopping = [];
+        $.each($("input[name='toppings']:checked"), function(){            
+            pizzatopping.push($(this).val());
+        });
+        console.log(pizzatopping.join(", "));
+        switch(pizzasize){
+          case "0":
+            price =0;
+          break;
+          case "large":
+             price = 1200;
+             console.log(price);
+           break;
+           case "medium":
+             price = 900;
+             console.log("The price is "+price);
+           break;
+           case "small":
+             price = 600;
+             console.log(price);
+           default:
+             console.log("error"); 
+         }
+         switch(pizzacrust){
+            case "0":
+              crust_price = 0;
+            break;
+            case "Crispy":
+              crust_price = 200;
+            break;
+            case "Stuffed":
+              crust_price = 250;
+            break;
+            case "Cheese Filled crust":
+              crust_price = 300;
+            break;
+            default:
+              console.log("No price"); 
+          }
+          let topping_value = pizzatopping.length*50;
+          console.log("toppins value" + topping_value);
+          total = price + crust_price + topping_value;
+          console.log(total);
+  
+          checkoutTotal = checkoutTotal + total;
+          console.log(checkoutTotal);
+        // constractor function
+        var newOrder = new Getpizza(pizzaname, pizzasize, pizzacrust,pizzatopping,total);
+  
+        $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+        console.log(newOrder);
      
